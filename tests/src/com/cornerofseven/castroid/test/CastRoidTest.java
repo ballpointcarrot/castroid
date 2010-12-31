a34a3541ae142c004e4cdc324aa5bdde125d37d7
Sean Mooney <bable4@gmail.com>
2010-12-31 14:31:50 -0600

    Adding a test project

---
+++ tests/src/com/cornerofseven/castroid/test/CastRoidTest.java
@@ -0,0 +1,12 @@
+package com.cornerofseven.castroid.test;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CastRoidTest {
+
+	@Test
+	public void dummyTest(){
+		fail("This is junk");
+	}
+}
