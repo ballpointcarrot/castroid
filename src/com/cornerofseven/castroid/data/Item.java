60f0f5ff4829197d659ea0822beab0e127746fce
Christopher Kruse <kruse.christopher@gmail.com>
2010-12-31 17:59:30 -0600

    Fixed ExpandableListView rendering.

--- src/com/cornerofseven/castroid/data/Item.java
+++ src/com/cornerofseven/castroid/data/Item.java
@@ -29,7 +29,7 @@ public final class Item implements BaseColumns{
     /**
      * How items are sorted by default
      */
-    public static final String DEFAULT_SORT = "createdDt DESC";
+    public static final String DEFAULT_SORT = "TITLE";
 
 
     /******************************
