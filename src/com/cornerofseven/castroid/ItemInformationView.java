4817c270efecaac1c38536ba308303dfc0bfae32
Sean Mooney <bable4@gmail.com>
2011-01-25 08:00:14 -0600

    Item information view activity working from main.

--- src/com/cornerofseven/castroid/ItemInformationView.java
+++ src/com/cornerofseven/castroid/ItemInformationView.java
@@ -16,7 +16,18 @@
 package com.cornerofseven.castroid;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.cornerofseven.castroid.data.Item;
+import com.cornerofseven.castroid.handlers.ChannelItemClickHandler;
 
 /**
  * @author Sean Mooney
@@ -24,19 +35,105 @@ import android.os.Bundle;
  */
 public class ItemInformationView extends Activity{
 
+	protected TextView mItemName, mItemDesc;
+	protected Button mPlay, mDownload;
+	
+	
+	
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.item_information_view);
 		
-		populateView();
+		collectWidgets();
+		
+		Intent intent = getIntent();
+		if(intent != null){
+			Uri dataUri = intent.getData();
+			if(dataUri != null){
+				bindData(dataUri);
+				
+				try{
+					long itemId = Long.parseLong(dataUri.getPathSegments().get(1));
+					bindListeners(itemId);
+				}catch(NumberFormatException nfe){
+					Toast.makeText(this, "Unknown item id in uri " 
+							+ dataUri.toString(), Toast.LENGTH_LONG).show();
+				}
+				
+			}
+		}
+		
 	}
 	
+
 	/**
 	 * Bind the elements from the view to the proper fields.
 	 */
-	protected void populateView(){
+	protected void collectWidgets(){
+		mItemName = (TextView)findViewById(R.id.iiv_item_name);
+		mItemDesc = (TextView)findViewById(R.id.iiv_item_desc);
 		
+		mPlay = (Button)findViewById(R.id.iiv_play);
+		mDownload = (Button)findViewById(R.id.iiv_download);
+	}
+	
+	/**
+	 * Bind the listeners to the buttons.
+	 * @param itemId, the database identifier for the item.
+	 */
+	protected void bindListeners(final long itemId) {
+		final Context context = this;
+		mPlay.setOnClickListener(new View.OnClickListener() {
+			
+			static final int ITEM_CLICK_PLAY = 1;
+			
+			/**
+			 * Handler for ChannelItem clicks.  We don't need
+			 * to view this item (we already are) so make the VIEW_ID -1;
+			 * 
+			 * In this activity, the play on click handler is the
+			 * only thing that will dispatch events to the 
+			 * ChannelItemClickHandler. This means we can make the 
+			 * object a field of the inner class, and not contribute
+			 * to polluting the state of the overall activity. Locality FTW. 
+			 */
+			final ChannelItemClickHandler itemClickHandler 
+				= new ChannelItemClickHandler(context, ITEM_CLICK_PLAY, -1);
+			
+			@Override
+			public final void onClick(View v) {
+				itemClickHandler.onItemClick(ITEM_CLICK_PLAY, itemId);
+			}
+		});
+	}
+
+	/**
+	 * Bind the data we want to show into the text views.
+	 * 
+	 * @param dataLocation Uri of item to display.
+	 */
+	protected void bindData(final Uri dataLocation) {
+		final String[] projection = {
+				Item.TITLE,
+				Item.DESC,
+				Item.PUB_DATE,
+		};
+		Cursor c = managedQuery(dataLocation, projection, null, null, null);
+	
+		if(c.moveToFirst()){
+			String title = c.getString(c.getColumnIndex(Item.TITLE));
+			String desc = c.getString(c.getColumnIndex(Item.DESC));
+			
+			//TODO: Display the publication date.
+			String pubDate = c.getString(c.getColumnIndex(Item.PUB_DATE));
+			
+			mItemName.setText(title);
+			mItemDesc.setText(desc);
+		}/*
+		  * else, didn't return anything, which implies the data location
+		  * was invalid. TODO: Do we warn/inform or just not display anything?
+		  */
 	}
 }
